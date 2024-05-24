9a1fcd301bfe9ffaa296f2d87df98e5c68a5444c
auser1337 <aaravsandhu10@outlook.com>
2024-05-23 18:59:38 -0700

    Never mind, that did nothing

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', function () {
     if (localStorage.getItem('dark-mode') === 'true') {
         document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
     }
 });
 
@@ -10,5 +8,4 @@ const toggleButton = document.querySelector('.dark-mode-toggle');
 
 toggleButton.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
-    localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
 });
