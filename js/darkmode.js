daaa92e15f71b23ff23590afcb30da00fd651c16
auser1337 <aaravsandhu10@outlook.com>
2024-05-23 18:57:36 -0700

    Fix(?) darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function () {
     if (localStorage.getItem('dark-mode') === 'true') {
         document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
     }
 });
 
@@ -8,4 +10,5 @@ const toggleButton = document.querySelector('.dark-mode-toggle');
 
 toggleButton.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
 });
